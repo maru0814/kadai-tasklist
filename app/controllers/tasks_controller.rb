8dbd7706bf6c35f9baced8b948edc852855f8772
Yu.Maruyama <taro.kirameki@techacademy.jp>
2020-08-06 07:40:55 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show,:edit,:destroy,:update]
     before_action :required_user_logged_in, only[:index]
+    before_action :correct_user, only: [:destroy]
   def index
-     @tasks=Task.all
+     if logged_in
+     
+     @tasks=current_user.tasks.order(id: :desc).page(params[:page])
+     end
   end
 
   def show
@@ -14,7 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task=Task.new(task_params)
+     @task=current_user.tasks.build(task_params)
      if @task.save
     flash[:success] = 'Taskが正常に投稿されました' 
      redirect_to @task
@@ -49,5 +53,11 @@ class TasksController < ApplicationController
   end
   def set_task
     @task=Task.find(params[:id])
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to login_url
+    end
   end
 end
